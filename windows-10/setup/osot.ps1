8c9e4e511678ea27be3395a6561a5e42eac2ced2
Mark Brookfield <mark@nl.mdb-lab.com@NL-UTC-P-LIN-01.nl.mdb-lab.com>
2019-08-12 14:17:53 +0200

    Added OSOT script

---
+++ windows-10/setup/osot.ps1
@@ -0,0 +1,30 @@
+$ErrorActionPreference = "Stop"
+
+$webserver = "intranet.mdb-lab.com"
+$url = "http://" + $webserver
+$installer = "VMware-Horizon-Agent-x86_64-7.9.0-13938590.exe"
+$listConfig = "/s /v ""/qn REBOOT=ReallySuppress ADDLOCAL=Core,RTAV,ClientDriveRedirection,VmwVaudio"""
+
+# Verify connectivity
+Test-Connection $webserver -Count 1
+
+# Get Horizon Agent
+Invoke-WebRequest -Uri ($url + "/" + $installer) -OutFile C:\$installer
+
+# Unblock installer
+Unblock-File C:\$installer -Confirm:$false -ErrorAction Stop
+
+# Install Horizon Agent
+Try 
+{
+   Start-Process C:\$installer -ArgumentList $listConfig -PassThru -Wait -ErrorAction Stop
+}
+Catch
+{
+   Write-Error "Failed to install the Horizon Agent"
+   Write-Error $_.Exception
+   Exit -1 
+}
+
+# Cleanup on aisle 4...
+Remove-Item C:\$installer -Confirm:$false
