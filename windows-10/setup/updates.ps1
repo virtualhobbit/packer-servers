81a6fc89783cb2ee42b58c41d9a692e2c4452e83
Mark Brookfield <virtualhobbit@users.noreply.github.com>
2019-10-02 15:30:10 +1000

    Automating VDI Template Creation with VMware Code Stream and HashiCorp Packer – Part 1: Building Windows

--- windows-10/setup/updates.ps1
+++ windows-10/setup/updates.ps1
@@ -11,7 +11,7 @@ Install-Module PSWindowsUpdate -Confirm:$false -Force
 Get-WindowsUpdate -Install -AcceptAll -IgnoreReboot | Out-File C:\result.txt
 
 # Sleepy time...
-Start-Sleep -Seconds 300
+Start-Sleep -Seconds 600
 
 # Run it again, as apparently it needs two attempts
 Get-WindowsUpdate -Install -AcceptAll -IgnoreReboot | Out-File C:\result2.txt
