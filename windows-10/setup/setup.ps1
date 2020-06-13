4ec09c037872b062786b9cc24c57f5dbfd7cbf75
Mark Brookfield <mark@mdb-lab.com>
2020-06-13 12:05:58 +0200

    Updated setup.ps1

--- windows-10/setup/setup.ps1
+++ windows-10/setup/setup.ps1
@@ -2,20 +2,29 @@ $ErrorActionPreference = "Stop"
 
 # Switch network connection to private mode
 # Required for WinRM firewall rules
-While ($profile.Name -ne "Network"){
+$profile = Get-NetConnectionProfile
+While ($profile.Name -eq "Identifying..."){
     Start-Sleep -Seconds 10
     $profile = Get-NetConnectionProfile
 }
 Set-NetConnectionProfile -Name $profile.Name -NetworkCategory Private
 
-# Enable Network Discovery
-netsh advfirewall firewall set rule group="Network Discovery" new enable=Yes
-# netsh advfirewall firewall set privateprofile rule group="Network Discovery" new enable=Yes
+# Disable Network Discovery
+# netsh advfirewall firewall set rule group="Network Discovery" new enable=No
+
+# Disanle Network Location Awareness
+# Set-Service -Name NlaSvc -StartupType Disabled
+
+# Enable PS Remoting
+# Enable-PSRemoting -SkipNetworkProfileCheck -Force
 
 # Enable WinRM service
 winrm quickconfig -quiet
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
+
+# Enable WinRM on Public profile
+Set-NetFirewallRule -Name 'WINRM-HTTP-In-TCP' -RemoteAddress Any -Enabled True
 
 # Reset auto logon count
 # https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-autologon-logoncount#logoncount-known-issue
