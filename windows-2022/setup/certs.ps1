d82eef528c41fb48bd40657a78861695cd3c4169
Mark Brookfield <mark@mdb-lab.com>
2021-03-05 20:46:03 +0100

    Added Windows Server 2022

---
+++ windows-2022/setup/certs.ps1
@@ -0,0 +1,25 @@
+$ErrorActionPreference = "Stop"
+
+$webserver = "intranet.mdb-lab.com"
+$url = "http://" + $webserver
+$certRoot = "root64.crt"
+$certIssuing = "issuing.crt"
+
+# Verify connectivity
+Test-Connection $webserver -Count 1
+
+# Get certificates
+ForEach ($cert in $certRoot,$certIssuing) {
+  Invoke-WebRequest -Uri ($url + "/" + $cert) -OutFile C:\$cert
+}
+
+# Import Root CA certificate
+Import-Certificate -FilePath C:\$certRoot -CertStoreLocation 'Cert:\LocalMachine\Root'
+
+# Import Issuing CA certificate
+Import-Certificate -FilePath C:\$certIssuing -CertStoreLocation 'Cert:\LocalMachine\CA'
+
+# Delete certificates
+ForEach ($cert in $certRoot,$certIssuing) {
+  Remove-Item C:\$cert -Confirm:$false
+}
